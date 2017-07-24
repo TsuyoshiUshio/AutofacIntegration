9d2fc7f6e4a801a9782f528f7be401b911dca162
TsuyoshiUshio <ushio@simplearchitect.com>
2017-07-23 23:11:54 -0700

    Appliying multiple filter

--- WebApiSample/Global.asax.cs
+++ WebApiSample/Global.asax.cs
@@ -20,10 +20,13 @@ namespace WebApiSample
             builder.RegisterType<ProductsController>().InstancePerRequest();
             builder.RegisterType<ConsoleLogger>().As<ILogger>();
             builder.Register(c => new LoggingActionFilter(c.Resolve<ILogger>()))
-                   //.AsWebApiActionFilterFor<ProductsController>(c => c.GetProduct(default(int)))
-				   .AsWebApiActionFilterFor<ProductsController>()
+                   .AsWebApiActionFilterFor<ProductsController>(c => c.GetProduct(default(int)))
+				//   .AsWebApiActionFilterFor<ProductsController>()
 				    //  .AsWebApiActionFilterFor<ProductsController>(c => c.GetAllProducts())
 																.InstancePerRequest();
+            builder.Register(c => new LoggingActionFilter(c.Resolve<ILogger>()))
+                 .AsWebApiActionFilterFor<ProductsController>(c => c.GetAllProducts())
+                   .InstancePerRequest();
             
             var container = builder.Build();
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);

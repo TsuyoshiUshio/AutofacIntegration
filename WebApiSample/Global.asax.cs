e49b3cb4bd3f45e4032283cf1be608d596aa0ae9
TsuyoshiUshio <ushio@simplearchitect.com>
2017-07-23 23:05:25 -0700

    Inital Commit

---
+++ WebApiSample/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using WebApiSample.Controllers;
+using WebApiSample.Filters;
+
+namespace WebApiSample
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var builder = new ContainerBuilder();
+            var config = GlobalConfiguration.Configuration;
+            // builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterWebApiFilterProvider(config);
+           
+            builder.RegisterType<ProductsController>().InstancePerRequest();
+            builder.RegisterType<ConsoleLogger>().As<ILogger>();
+            builder.Register(c => new LoggingActionFilter(c.Resolve<ILogger>()))
+                   //.AsWebApiActionFilterFor<ProductsController>(c => c.GetProduct(default(int)))
+				   .AsWebApiActionFilterFor<ProductsController>()
+				    //  .AsWebApiActionFilterFor<ProductsController>(c => c.GetAllProducts())
+																.InstancePerRequest();
+            
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
